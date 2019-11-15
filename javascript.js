df958f92cca5c5347618ade5d834dc52210c569b
Jonathan Engman <jonathanengman@hotmail.com>
2019-11-15 05:05:43 +0100

    JS DONE
    All complete, at least the JS

--- javascript.js
+++ javascript.js
@@ -1,7 +1,50 @@
-import '../styles/index.scss';
-import anime from 'animejs'
+let data = '{"results":[{"youtube":{"music":"https://www.youtube.com/embed/s4iLX7eWwmE"}}, {"youtube":{"music":"https://www.youtube.com/embed/HUKsUtHx4QU"}}, {"youtube":{"music":"https://www.youtube.com/embed/_14cb1xCY-4"}}, {"youtube":{"music":"https://www.youtube.com/embed/EAYfJckSEN0"}}, {"youtube":{"music":"https://www.youtube.com/embed/JcYVe5u1Cms"}}]}'
+let parsedData = JSON.parse(data)
 
-anime({
-    targets: '.tester',
-    translateX: 250
-  });
+function formatName(song){
+        return song
+}
+
+let users = parsedData.results
+let currentIndex = 0
+let currentUser = users[currentIndex]
+
+
+
+function update(){
+    let video = document.querySelector(".videoPlayer"); 
+    video.setAttribute("src", currentUser.youtube.music)
+}
+
+let button1 = document.querySelector(".previous")
+let button2 = document.querySelector(".next")
+
+function previous(){
+    currentIndex--
+    if(currentIndex < 0) {
+        currentIndex = users.length -1;
+    }
+    currentUser = users[currentIndex]
+    update()
+}
+
+function next(){
+    currentIndex++
+    if(currentIndex >= users.length) {
+        currentIndex = 0;
+    }
+    currentUser = users[currentIndex]
+    update()
+}
+
+button1.addEventListener("click", function() {
+    previous()
+});
+
+update();
+
+button2.addEventListener("click", function() {
+    next()
+});
+
+update();
